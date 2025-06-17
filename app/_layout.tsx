90346f5e9b92a386d83ce71151c65117a142c646
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2025-06-17 11:49:27 +0530

    changed stack navigator to completely bottom tab navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import { StyleSheet } from "react-native";
 
 export default function Layout() {
@@ -8,7 +8,7 @@ export default function Layout() {
       colors={["#0a0a0a", "#1a1a1a", "#000"]}
       style={styles.container}
     >
-      <Stack screenOptions={{ headerShown: false }} />
+      <Slot screenOptions={{ headerShown: false }} />
     </LinearGradient>
   );
 }
